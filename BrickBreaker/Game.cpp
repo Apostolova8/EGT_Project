a188223dc54cb8c1ae2c203d23e02bb5a04db1d9
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-23 16:45:42 +0200

    Update Game.cpp

--- BrickBreaker/Game.cpp
+++ BrickBreaker/Game.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <SDL.h>
 #include "SDL_image.h"
+#include "Bricks.h"
 
 using namespace std;
+
+Bricks* bricks = new Bricks();
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags) {
 	cout << "SDL init success\n";
@@ -29,18 +32,7 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 				TextureManager::Instance()->loadTexture("images/brickY.jpg", "brickY", renderer);
 
 				//load brick positions:
-				for (int i = 0; i < 5; i++) { //create 5 bricks
-					SDL_Rect brickRect = { 50 + i * 100, 50, brickWidth, brickHeight };
-									//brick first x position + spacing, brick's row y position
-					brickPos.push_back(brickRect);	//add bricks positions to the vector
-					brickV[i] = true; //bricks are visible when they are created
-				}
-
-				for (int i = 5; i < 10; i++) {
-					SDL_Rect brickRect = { 100 + (i - 5) * 100, 100, brickWidth, brickHeight };
-					brickPos.push_back(brickRect);
-					brickV[i] = true; 
-				}
+				bricks->loadBricks();
 			}
 
 			else {
@@ -72,22 +64,16 @@ void Game::render() {
 	SDL_GetWindowSize(window, &ww, &wh);
 
 	TextureManager::Instance()->drawTexture("startButton", 150, 150, 300, 120, renderer);	//start button
-	//TextureManager::Instance()->drawOneFrameFromTexture("1", 0, 0, 130, 130, 2, currentFrame, renderer);
 
 	if (startButton == true) {	//if the start button is clicked (is true), show the background
 		TextureManager::Instance()->drawTexture("background", backgroundXPos, backgroundYPos, backgroundWidth, backgroundHeight, renderer);	//background
-		TextureManager::Instance()->drawTexture("paddle", paddleXPos, paddleYPos, paddleWidth, paddleHeight, renderer); //paddle
+		TextureManager::Instance()->drawTexture("paddle", paddle.getPaddleXPos(), paddle.getPaddleYPos(), paddle.getPaddleWidth(), paddle.getPaddleHeight(), renderer); //paddle
 		TextureManager::Instance()->drawTexture("wall", leftWallXPos, leftWallYPos, leftWallWidth, leftWallHeight, renderer); //left wall
 		TextureManager::Instance()->drawTexture("wall", rightWallXPos, rightWallYPos, rightWallWidth, rightWallHeight, renderer); //right wall
 		TextureManager::Instance()->drawTexture("ball", ball.getBallXPos(), ball.getBallYPos(), ball.getBallWidth(), ball.getBallHeight(), renderer); //ball
 
 		//draw bricks:
-		for (int i = 0; i < brickPos.size(); i++) { //iterate over the elements in vector
-			if (brickV[i]) { //if the brick is visible (brickV = true)
-				SDL_Rect brickRect = brickPos[i]; //create rect which contains the brick "elements"
-				TextureManager::Instance()->drawTexture("brickY", brickRect.x, brickRect.y, brickRect.w, brickRect.h, renderer);
-			}
-		}
+		bricks->drawBricks(renderer);
 	}
 
 	SDL_RenderPresent(renderer);
@@ -100,19 +86,18 @@ void Game::handleEvents() {
 		case SDL_QUIT:
 			running = false;
 			break;
-		case SDL_MOUSEBUTTONDOWN:	//click start button
+		case SDL_MOUSEBUTTONDOWN:
 			int mouseX;
 			int mouseY;
 			SDL_GetMouseState(&mouseX, &mouseY);
-			if (startButton == true) {
+			if (startButton == true) {	//ball start bouncing if user click on the screen if start button is clicked
 				ball.setBallXSpeed(1);
 				ball.setBallYSpeed(1);
 			}
 			else
 			{
-				//paddleXPos = mouseX - paddleWidth / 2;
 				if (mouseX > 150 && mouseX < 450 && mouseY > 150 && mouseY < 270) {
-					// Check if the mouse click is on the start button
+					//check if the mouse click is on the start button
 					startButton = true;
 				}
 			}
@@ -120,13 +105,19 @@ void Game::handleEvents() {
 		case SDL_KEYDOWN:	//move paddle
 			switch (event.key.keysym.sym) {
 			case SDLK_LEFT:
-				if (paddleXPos > 25) {
-					paddleXPos -= 15; //left moving
+				if (paddle.getPaddleXPos() == 15) {
+					paddle.stopMovingPaddle = false;	//stop moving paddle when x-position of paddle reaches 15 (to the left wall)
+				}
+				else {
+					paddle.setPaddleXPos(paddle.getPaddleXPos() - 15); //left moving
 				}
 				break;
 			case SDLK_RIGHT:
-				if (paddleXPos + paddleWidth  < 575) {
-					paddleXPos += 15; //right moving
+				if (paddle.getPaddleXPos() == 465) {
+					paddle.stopMovingPaddle = false;	//stop moving paddle when x-position of paddle reaches 465 (to the right wall)
+				}
+				else {
+					paddle.setPaddleXPos(paddle.getPaddleXPos() + 15); //right moving
 				}
 				break;
 			default:
@@ -138,43 +129,23 @@ void Game::handleEvents() {
 
 void Game::movePaddle(int x, int y)
 {
-	paddleXPos += x;
-	paddleYPos += y;
+	paddle.setPaddleXPos(paddle.getPaddleXPos() + x);
+	paddle.setPaddleYPos(paddle.getPaddleXPos() + y);
 }
-
-//bool Game::checkCollision(int ballX, int ballY, int ballW, int ballH, int x, int y, int w, int h)
-//{
-//	if (ballY + ballH > y && ballX + ballW > x && ballX < x + w && ballY < y + h) {
-//		return true;
-//	}
-//	
-//	return false;
-//}
 
 void Game::update() {
 
-	ball.ballSpeed();
+	//update ball position with speed:
+	ball.ballSpeed();	
 
-	//cout << "Ball X Position: " << ballXPos << endl;
-	//cout << "Ball Y Position: " << ballYPos << endl;
-
-	//collision with walls
+	//ball collision with walls:
 	ball.collissionWalls();
 
-	//collision with paddle
-
-	//collision with bricks
-	ball.checkCollision(paddleXPos, paddleYPos, paddleWidth, paddleHeight);
-	for (int i = 0; i < brickPos.size(); i++) {	//every brick in vector
-		if (brickV[i])
-		{
-			if (ball.checkCollision(brickPos[i].x, brickPos[i].y, brickPos[i].w, brickPos[i].h))
-			{
-				brickV[i] = false;	//hide the brick, because it was hit by ball
-				cout << "ball hit brick" << endl;
-			}
-		}
-	}
+	//ball collision with paddle:
+	ball.checkCollision(paddle.getPaddleXPos(),paddle.getPaddleYPos(), paddle.getPaddleWidth(), paddle.getPaddleHeight());
+	
+	//ball collision with bricks:
+	bricks->checkCollision(ball.getBallXPos(), ball.getBallYPos(), ball.getBallWidth(), ball.getBallHeight());
 }
 
 void Game::clean() {
@@ -192,6 +163,7 @@ Game::Game() {
 	Game::window = NULL;
 	Game::renderer = NULL;
 	Game::running = true;
+	Game::currentFrame = 4;
 }
 
 Game::~Game()
