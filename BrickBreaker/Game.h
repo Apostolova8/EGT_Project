34e9c928869916b043129d984fec1c17ad0b3f71
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-23 16:45:13 +0200

    Update Game.h

--- BrickBreaker/Game.h
+++ BrickBreaker/Game.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "Ball.h"
+#include "Paddle.h"
 
 class Game
 {
@@ -15,7 +16,6 @@ public:
 	void handleEvents();
 	void clean();
 	bool isRunning();
-
 	void movePaddle(int x, int y);
 
 private:
@@ -23,45 +23,33 @@ private:
 	SDL_Renderer* renderer = NULL;
 	SDL_Texture* texture = NULL;
 	bool running;
+	int currentFrame;
 
 	bool startButton = false; //is the start button clicked
 
-	//bool checkCollision(int ballX, int ballY, int ballW, int ballH, int x, int y, int w, int h);
+	bool stopMoving = true;
 
-	//
-	//int ballXPos = 275; //x-position of ball (left edge)
-	//int ballYPos = 330; //y-position of ball (top edge)
-	//int ballWidth = 20; //ball width
-	//int ballHeight = 20; //ball height
-	//int ballXSpeed = 1; //velocity of the ball
-	//int ballYSpeed = 1;
-
-	int paddleXPos = 225; //x-position of paddle (left edge)
-	int paddleYPos = 340; //y-position of paddle (top edge)
-	int paddleWidth = 120;	//width
-	int paddleHeight = 30;	//height
-
+	//background:
 	int backgroundXPos = 0; //x-position of background (left edge)
 	int backgroundYPos = 0; //y-position of background (top edge)
 	int backgroundWidth = 600; //background width
 	int backgroundHeight = 400; //background height
 
+	//left wall:
 	int leftWallXPos = 0; //x-position of left wall (left edge)
 	int leftWallYPos = 0; //y-position of left wall (top edge)
 	int leftWallWidth = 25; //left wall width
 	int leftWallHeight = 400; //left wall height
 
+	//right wall:
 	int rightWallXPos = 575; //x-position of left wall (left edge)
 	int rightWallYPos = 1; //y-position of left wall (top edge)
 	int rightWallWidth = 25; //left wall width
 	int rightWallHeight = 400; //left wall height
-
-	std::vector<SDL_Rect> brickPos; //hold bricks positions
-	bool brickV[10]; //which brick is visible
-	int brickHeight = 50;
-	int brickWidth = 50; 
-
+	
 	Ball ball;
+
+	Paddle paddle;
 
 	int lives = 2;
 };
