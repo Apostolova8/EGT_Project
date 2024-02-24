47fa4bc9d8630e91f4a53f8d2ad4b6c1ec44784e
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-24 15:14:42 +0200

    Update Game.cpp

--- BrickBreaker/Game.cpp
+++ BrickBreaker/Game.cpp
@@ -15,7 +15,7 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 	cout << "SDL init success\n";
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-		// Create window
+		//Create window
 		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		if (window != 0) {	// window init success
 			cout << "Window creation success\n";
@@ -34,11 +34,14 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 				TextureManager::Instance()->loadTexture("images/brickG.jpg", "brickG", renderer);
 				TextureManager::Instance()->loadTexture("images/brickY.jpg", "brickY", renderer);
 				TextureManager::Instance()->loadTexture("images/lives.jpg", "lives", renderer);
-				TextureManager::Instance()->loadTexture("images/gameOver.jpg", "gameOver", renderer);
-
 
 				//load brick positions:
 				bricks->loadBricksPositions();
+			}
+
+			if (!ttf_init()) {
+				cout << "Failed to initialize TTF\n";
+				return false;
 			}
 
 			else {
@@ -74,15 +77,17 @@ void Game::render() {
 	case PLAY:	//case play -> render all textures by their objects, after clicked start button
 		if (startButton == true) {
 			screen->drawAll(renderer);
-			SDL_RenderCopy(renderer, textTextureFont1, NULL, &dRectFont1);
 			ball.drawBall(renderer);
 			paddle.drawPaddle(renderer);
 			bricks->drawBricks(renderer);
+			SDL_RenderCopy(renderer, textTextureFont1, NULL, &dRectFont1);
+			SDL_RenderCopy(renderer, textTextureFont2, NULL, &dRectFont2);
 		}
 		break;
 	case GAME_OVER:	//if lives = 0, game over
 		if (ball.getLives() <= 0) {
-			TextureManager::Instance()->drawTexture("gameOver", 0, 0, 600, 400, renderer); //game over texture
+			//SDL_RenderCopy(renderer, textTextureFont3, NULL, &dRectFont3);
+			//running = false;
 		}
 		break;
 	}
@@ -137,7 +142,6 @@ void Game::handleEvents() {
 			}
 			break;
 		}
-
 		if (event.button.button == SDL_BUTTON_LEFT) {	//left mouse click on screen
 			switch (gameState) {
 			case START:
@@ -148,11 +152,13 @@ void Game::handleEvents() {
 			case PLAY:	// -//- load textures
 				break;
 			case GAME_OVER:	//if game ends, set the state to start, set 2 lives, load all bricks and get ball x and y position
-					gameState = START;
-					ball.setLives(2); 
-					bricks->loadBricksPositions(); 
-					ball.getBallXPos();
-					ball.getBallYPos();
+				gameState = START;
+				ball.setLives(2);
+				bricks->loadBricksPositions();
+				SDL_RenderCopy(renderer, textTextureFont1, NULL, &dRectFont1);
+				SDL_RenderCopy(renderer, textTextureFont2, NULL, &dRectFont2);
+				ball.getBallXPos();
+				ball.getBallYPos();
 				break;
 			}
 		}
@@ -165,30 +171,48 @@ void Game::movePaddle(int x, int y)
 	paddle.setPaddleYPos(paddle.getPaddleXPos() + y);
 }
 
-bool Game::ttf_init()	//for text
+bool Game::ttf_init() //for text
 {
 	if (TTF_Init() == -1) {
 		return false;
 	}
-	TTF_Font* font1 = TTF_OpenFont("text/Arcade.ttf", 35);
 
-	if (font1 == NULL) {
+	TTF_Font* font1 = TTF_OpenFont("text/Arcade.ttf", 50);
+	TTF_Font* font2 = TTF_OpenFont("text/Arcade.ttf", 50);
+	///TTF_Font* font3 = TTF_OpenFont("text/Arcade.ttf", 300);
+
+	if (font1 == NULL || font2 == NULL /*font3 == NULL*/) {
 		return false;
 	}
 
 	SDL_Surface* tempLivesText = NULL;
+	SDL_Surface* tempScoreText = NULL;
+	//SDL_Surface* tempGOText = NULL;
 
-	tempLivesText = TTF_RenderText_Blended(font1, to_string(ball.getLives()).c_str(), { 255,255,255,255 });
-	//tempSurfaceText = TTF_RenderText_Blended(font1, "x", { 255,255,255,255 });
+	tempLivesText = TTF_RenderText_Blended(font1, to_string(ball.getLives()).c_str(), { 255, 255, 255, 255 });
+	tempScoreText = TTF_RenderText_Blended(font2, to_string(bricks->getPoints()).c_str(), { 255, 255, 255, 255 });
+	
+	//std::string gameOverText = "Game Over"; 
+	//tempGOText = TTF_RenderText_Blended(font3, gameOverText.c_str(), { 255, 255, 255, 255 });
 
 	textTextureFont1 = SDL_CreateTextureFromSurface(renderer, tempLivesText);
+	textTextureFont2 = SDL_CreateTextureFromSurface(renderer, tempScoreText);
+	//textTextureFont3 = SDL_CreateTextureFromSurface(renderer, tempGOText);
 
 	int tw, th;
 	SDL_QueryTexture(textTextureFont1, 0, 0, &tw, &th);
-	dRectFont1 = { 530, 8, tw, th };
+	dRectFont1 = { 540, 6, tw, th };
+	SDL_QueryTexture(textTextureFont2, 0, 0, &tw, &th);
+	dRectFont2 = { 300, 8, tw, th };
+	//SDL_QueryTexture(textTextureFont3, 0, 0, &tw, &th);
+	//dRectFont3 = { 300, 200, tw, th };
 
 	SDL_FreeSurface(tempLivesText);
 	TTF_CloseFont(font1);
+	SDL_FreeSurface(tempScoreText);
+	TTF_CloseFont(font2);
+	//SDL_FreeSurface(tempGOText); 
+	//TTF_CloseFont(font3);
 
 	return true;
 }
@@ -196,10 +220,10 @@ bool Game::ttf_init()	//for text
 void Game::update() {
 
 	//update ball position with speed:
-	ball.ballSpeed();	
+	ball.ballSpeed();
 
 	//ball collision with paddle:
-	ball.checkCollision(paddle.getPaddleXPos(),paddle.getPaddleYPos(), paddle.getPaddleWidth(), paddle.getPaddleHeight());
+	ball.checkCollision(paddle.getPaddleXPos(), paddle.getPaddleYPos(), paddle.getPaddleWidth(), paddle.getPaddleHeight());
 
 	//ball collision with walls:
 	ball.collissionWalls();
@@ -208,12 +232,10 @@ void Game::update() {
 	bricks->checkCollision(ball.getBallXPos(), ball.getBallYPos(), ball.getBallWidth(), ball.getBallHeight());
 
 	//update game state:
-	switch (gameState) {	
+	switch (gameState) {
 	case START:
 		break;
 	case PLAY:
-		ball.getBallXPos();
-		ball.getBallYPos();
 		if (ball.getLives() <= 0) {	//if lives = 0; game state = game over
 			gameState = GAME_OVER;
 		}
@@ -234,7 +256,7 @@ bool Game::isRunning() {
 	return Game::running;
 }
 
-Game::Game() : gameState(START), lives(2){
+Game::Game() : gameState(START), lives(2) {
 	Game::window = NULL;
 	Game::renderer = NULL;
 	Game::running = true;
