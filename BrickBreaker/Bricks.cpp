6fc728474c51059d70b3133e4779301eaf6fd846
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-24 20:26:11 +0200

    Update Bricks.cpp

--- BrickBreaker/Bricks.cpp
+++ BrickBreaker/Bricks.cpp
@@ -3,7 +3,6 @@
 #include "Game.h"
 #include "SDL.h"
 #include "Ball.h"
-#include "Game.h"
 
 Bricks::Bricks()
 {
@@ -13,19 +12,19 @@ void Bricks::loadBricksPositions() {
     //yellow bricks in first row:
     for (int i = 0; i < 5; i++) {
         SDL_Rect brickRect = { 50 + i * 100, 50, brickWidth, brickHeight };
-        brickPos.push_back(Brick(brickRect, true, 1)); //true visability and 1 hit for yellow bricks
+        brickPos.push_back(Brick(brickRect, true, 1)); //true visibility and 1 hit for yellow bricks
     }
 
     //gray bricks in second row:
     for (int i = 5; i < 10; i++) {
         SDL_Rect brickRect = { 100 + (i - 5) * 100, 100, brickWidth, brickHeight };
-        brickPos.push_back(Brick(brickRect, true, 2)); //true visability and 2 hits for yellow bricks
+        brickPos.push_back(Brick(brickRect, true, 2)); //true visibility and 2 hits for gray bricks
     }
 }
 
 void Bricks::drawBricks(SDL_Renderer* renderer) {
     for (int i = 0; i < brickPos.size(); i++) {
-        if (brickPos[i].isVisible) {    //if visability is true
+        if (brickPos[i].isVisible) {    //if visibility is true
             SDL_Rect brickRect = brickPos[i].rect;
             if (brickPos[i].hitCount == 1) {    //draw yellow brick if the hit count is 1
                 TextureManager::Instance()->drawTexture("brickY", brickRect.x, brickRect.y, brickRect.w, brickRect.h, renderer);
@@ -37,7 +36,7 @@ void Bricks::drawBricks(SDL_Renderer* renderer) {
     }
 }
 
-bool Bricks::checkCollision(double x, double y, int w, int h) {
+bool Bricks::checkCollision(double x, double y, double& ys, double& xs, int w, int h) {
 
     for (int i = 0; i < brickPos.size(); i++) { //for all of the bricks
         if (brickPos[i].isVisible) {    //if bricks are visible
@@ -45,22 +44,22 @@ bool Bricks::checkCollision(double x, double y, int w, int h) {
                 && x < brickPos[i].rect.x + brickPos[i].rect.w && y < brickPos[i].rect.y + brickPos[i].rect.h) {
 
                 if (brickPos[i].hitCount == 2) { //gray brick
-                    brickPos[i].hitCount--; //turn gray brick into yellow
+                    ys = -ys;
+                    xs = -xs;
+                    brickPos[i].hitCount -= 1; //turn gray brick into yellow
                     points += 1;
                     cout << "hit gray brick\n";
                 }
-                else if (brickPos[i].hitCount == 0) { //gray brick
-                    brickPos[i].isVisible = false; //turn gray brick into yellow
-                    cout << "hit gray brick\n";
+                else if (brickPos[i].hitCount == 1) { //yellow brick
+                    ys = -ys;
+                    points += 1;
+                    brickPos[i].isVisible = false;
+                    cout << "hit yellow brick\n";
                 }
-                //else if (brickPos[i].hitCount == 0) { 
-                //    brickPos[i].isVisible == false;
-                //    cout << "hit gray brick\n";
-                //}
             }
         }
     }
-    return false; 
+    return false;
 }
 
 
