ddc0e4ae8b940caeeaebeedee0c0c55d31db67c3
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-24 13:51:33 +0200

    Update Bricks.cpp

--- BrickBreaker/Bricks.cpp
+++ BrickBreaker/Bricks.cpp
@@ -10,50 +10,54 @@ Bricks::Bricks()
 }
 
 void Bricks::loadBricksPositions() {
-
-    for (int i = 0; i < 5; i++) { //create 5 bricks
-        SDL_Rect brickRect = { 50 + i * 100, 50, brickWidth, brickHeight }; //first brick x pos, spacing, x-pos of whole row
-        brickPos.push_back(brickRect); //add bricks positions to the vector
-        brickV[i] = true;        //bricks are visible when they are created
-
+    //yellow bricks in first row:
+    for (int i = 0; i < 5; i++) {
+        SDL_Rect brickRect = { 50 + i * 100, 50, brickWidth, brickHeight };
+        brickPos.push_back(Brick(brickRect, true, 1)); //true visability and 1 hit for yellow bricks
     }
 
+    //gray bricks in second row:
     for (int i = 5; i < 10; i++) {
         SDL_Rect brickRect = { 100 + (i - 5) * 100, 100, brickWidth, brickHeight };
-        brickPos.push_back(brickRect);
-        brickV[i] = true;
+        brickPos.push_back(Brick(brickRect, true, 2)); //true visability and 2 hits for yellow bricks
     }
 }
 
 void Bricks::drawBricks(SDL_Renderer* renderer) {
-    for (int i = 0; i < 5; i++) { // iterate over the elements in vector
-        if (brickV[i]) { // if the brick is visible (brickV = true)
-            SDL_Rect brickRect = brickPos[i]; // create rect which contains the brick "elements"
-            TextureManager::Instance()->drawTexture("brickY", brickRect.x, brickRect.y, brickRect.w, brickRect.h, renderer);
-        }
-    }
-    for (int i = 5; i < 10; i++) { // iterate over the elements in vector
-        if (brickV[i]) { // if the brick is visible (brickV = true)
-            SDL_Rect brickRect = brickPos[i]; // create rect which contains the brick "elements"
-            TextureManager::Instance()->drawTexture("brickG", brickRect.x, brickRect.y, brickRect.w, brickRect.h, renderer);
-        }
-    }
-}
-
-bool Bricks::checkCollision(double ballX, double ballY, int ballW, int ballH) {
-    for (int i = 0; i < brickPos.size(); i++) {	//every brick in vector
-        if (brickV[i])
-        {
-            if (ballY + ballH > brickPos[i].y && ballX + ballW > brickPos[i].x
-                && ballX < brickPos[i].x + brickPos[i].w && ballY < brickPos[i].y + brickPos[i].h) {
-
-                brickV[i] = false;	//hide the brick, because it was hit by ball
-                cout << "ball hit brick" << endl;
+    for (int i = 0; i < brickPos.size(); i++) {
+        if (brickPos[i].isVisible) {    //if visability is true
+            SDL_Rect brickRect = brickPos[i].rect;
+            if (brickPos[i].hitCount == 1) {    //draw yellow brick if the hit count is 1
+                TextureManager::Instance()->drawTexture("brickY", brickRect.x, brickRect.y, brickRect.w, brickRect.h, renderer);
+            }
+            if (brickPos[i].hitCount == 2) {   //draw gray brick if the hit count is 2
+                TextureManager::Instance()->drawTexture("brickG", brickRect.x, brickRect.y, brickRect.w, brickRect.h, renderer);
             }
         }
     }
-    return false;
 }
+
+bool Bricks::checkCollision(double x, double y, int w, int h) {
+
+    for (int i = 0; i < brickPos.size(); i++) { //for all of the bricks
+        if (brickPos[i].isVisible) {    //if bricks are visible
+            if (y + h > brickPos[i].rect.y && x + w > brickPos[i].rect.x    //check collision with ball
+                && x < brickPos[i].rect.x + brickPos[i].rect.w && y < brickPos[i].rect.y + brickPos[i].rect.h) {
+
+                if (brickPos[i].hitCount == 2) { //gray brick
+                    brickPos[i].hitCount--; //turn gray brick into yellow
+                    cout << "hit gray brick\n";
+                }
+                else if (brickPos[i].hitCount <= 0) {
+                    brickPos[i].isVisible = false; // Hide the yellow brick if hit count reaches 0
+                    std::cout << "Ball hit brick" << std::endl;
+                }
+            }
+        }
+    }
+    return false; 
+}
+
 
 int Bricks::getBrickHeight() const {
     return brickHeight;
