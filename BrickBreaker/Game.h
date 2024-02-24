54bc37b36a39531624ee9eb020aadec736618d40
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-24 15:14:15 +0200

    Update Game.h

--- BrickBreaker/Game.h
+++ BrickBreaker/Game.h
@@ -7,7 +7,7 @@
 #include "SDL_ttf.h"
 #include "SDL.h"
 
-enum GameState {	//gamep set
+enum GameState {  //game set
 	START,
 	PLAY,
 	GAME_OVER
@@ -31,20 +31,29 @@ private:
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
 	SDL_Texture* texture = NULL;
+
+	//lives:
 	SDL_Texture* textTextureFont1;
 	SDL_Rect dRectFont1;
+	//points:
+	SDL_Texture* textTextureFont2;
+	SDL_Rect dRectFont2;
+	//game over:
+	//SDL_Texture* textTextureFont3;
+	//SDL_Rect dRectFont3;
 
 	bool running;
 
 	bool startButton = false; //is the start button clicked
 
 	bool stopMoving = true;
-	
+
 	//objects
 	Ball ball;
 	Paddle paddle;
 	GameState gameState;
-	
+
 	int lives;
+	int points;
 
 };
